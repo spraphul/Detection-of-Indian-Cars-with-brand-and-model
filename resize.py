8a5b1badb5e51354e621f790d277fa3e8c7da4e1
Praphul Singh <praphulsingh1996@gmail.com>
2017-07-18 23:08:15 +0530

    Add files via upload

---
+++ resize.py
@@ -0,0 +1,30 @@
+
+import PIL
+from PIL import Image
+import os, sys
+
+def resizeImage(infile, output_dir="", size=(200,200)):
+     outfile = os.path.splitext(infile)[0]+"_resized"
+     extension = os.path.splitext(infile)[1]
+
+     #if (cmp(extension, ".jpg")):
+      #  return
+
+     if infile != outfile:
+        try :
+            im = Image.open(infile)
+            im.resize(size, Image.ANTIALIAS)
+            im.save(output_dir+outfile+extension,"jpg")
+        except IOError:
+            print("cannot reduce image for ", infile)
+
+
+if __name__=="__main__":
+    output_dir = "Paul"
+    dir = "/home/praphulr/DeepLogo-master/Images/Images"
+
+    if not os.path.exists(os.path.join(dir,output_dir)):
+        os.mkdir(output_dir)
+
+    for file in os.listdir(dir):
+        resizeImage(file,output_dir)
