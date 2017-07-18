83680b4bf2527deb669d74b3e9d8c4050fa7a337
Praphul Singh <praphulsingh1996@gmail.com>
2017-07-19 00:00:11 +0530

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,3 +1,9 @@
+# Copyright 2017 The Impetuors Authors. All Rights Reserved.
+# Authors:
+# Preetham Paul Sunkari, Praphul Singh, Harshit Saini, Aadrish Sharma
+# Limited under the License.
+##########################################################################################
+
 import os
 path = '/home/praphulr/Chevrolet'
 files = os.listdir(path)
