da60c9af242d77d92c72b8d19a2d322197ba2d23
Praphul Singh <praphulsingh1996@gmail.com>
2017-07-19 00:00:35 +0530

    Update resize.py

--- resize.py
+++ resize.py
@@ -1,4 +1,8 @@
-
+# Copyright 2017 The Impetuors Authors. All Rights Reserved.
+# Authors:
+# Preetham Paul Sunkari, Praphul Singh, Harshit Saini, Aadrish Sharma
+# Limited under the License.
+##########################################################################################
 import PIL
 from PIL import Image
 import os, sys
